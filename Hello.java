3840137c5eeb6b4a2e46801d8cf7ca5d82c4ec97
ajayravriya <ajayravriya14325@gmail.com>
2023-11-21 18:17:27 +0530

    #9 Assignment Operators in java

--- Hello.java
+++ Hello.java
@@ -4,11 +4,15 @@
 class Hello {
   public static void main(String args[])
   {
-    //Pramoted conversion
-    byte a = 10;
-    byte b = 30;
+    //Assignment Operators
+    int a = 7;
+    int b = 5;
 
-    int result = a * b; //type pramotion
+    int result = a + b;
+    //int result = a - b; Output 2
+    //int result = a * b; Output 35
+    //int result = a / b; Output 1
+    //int result = a % b; Output 2
 
     System.out.println(result);
     
