ce4273de37a46c4e04c4d27ee05e60e64efd1b78
ajayravriya <ajayravriya14325@gmail.com>
2023-11-21 14:09:29 +0530

    #6 Data types in java

--- Hello.java
+++ Hello.java
@@ -4,9 +4,18 @@
 class Hello {
   public static void main(String a[])
   {
+    //primitive data types
     int num1 = 3;
-    int num2 = 5;
-    int result = num1 + num2;
-    System.out.println(result);
+    byte by = 127;
+    short sh = 558;
+    long l = 5854l;
+
+    float f = 5.8f;
+    double d = 5.8;
+
+    char c = 'k';
+
+    boolean b = true;
+    
   }
 }
