74dd3201ed2f01da88bfcce13306d8cb8c1c98e3
ajayravriya <ajayravriya14325@gmail.com>
2023-11-27 22:04:18 +0530

    #11 Logical Operator in java

---
+++ Logical.java
@@ -0,0 +1,12 @@
+public class Logical {
+  public static void main(String args[])
+  {
+    int x = 14;
+    int y = 20;
+    int a = 5;
+    int b = 2;
+
+    boolean result = x > y && a < b;
+    System.out.println(result);
+  }
+}
