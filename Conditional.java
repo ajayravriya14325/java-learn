494f0bd835bd3eb0126b6da0b50c78ebbc93e96b
ajayravriya <ajayravriya14325@gmail.com>
2023-12-05 22:24:35 +0530

    else if statement

--- Conditional.java
+++ Conditional.java
@@ -1,14 +1,23 @@
 public class Conditional {
   public static void main(String args[])
   {
-    int x=9;
-    int y=7;
-    if(x>y)
+    int x=17;
+    int y=13;
+    int z=33;
+    
+    if( x<y && x<z) 
+    { 
+      System.out.println(x);
+    }
+    else if(y<z)
     {
-     System.out.println(x);
-     //System.out.println("Thank you");
+      System.out.println(y);
     }
     else
-      System.out.println(y);
+    {
+      System.out.println(z);
+    }
+    
+    
   }
 }
