2a4658786cf05e4537da88b40082e614b4ba75c7
ajayravriya <ajayravriya14325@gmail.com>
2023-12-17 14:29:22 +0530

    Class and object in java

---
+++ DemoClass.java
@@ -0,0 +1,27 @@
+class Calculator {
+  int a;
+
+  public int add(int n1,int n2) {
+    
+    int r = n1 + n2;
+    return r;
+  }
+  
+}
+
+
+public class DemoClass {
+  public static void main(String args[]){
+
+    int num1 = 10;
+    int num2 = 30;
+    
+    Calculator calc = new Calculator();
+
+    int result = calc.add(num1,num2);
+
+    System.out.println(result);
+
+
+  }
+}
