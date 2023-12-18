8d6a3c3da948613bf4a959deb7de6e2aa52f33bc
ajayravriya <ajayravriya14325@gmail.com>
2023-12-18 18:10:05 +0530

    Array print in java

--- Array.java
+++ Array.java
@@ -1,9 +1,15 @@
 public class Array {
   public static void main(String args[]){
     int num[] = new int[5];
+    num[0] =7;
+    num[1] = 6;
+    num[2] = 10;
+    num[3] = 20; 
 
-    //num[1] = 6;
-
+    System.out.println(num[0]);
     System.out.println(num[1]);
+    System.out.println(num[2]);
+    System.out.println(num[3]);
+    
   }
 }
