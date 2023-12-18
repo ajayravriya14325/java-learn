f4be489913e45b53076d4d3af3db27b843b64485
ajayravriya <ajayravriya14325@gmail.com>
2023-12-18 18:07:24 +0530

    Array initialise in java current value 0

--- Array.java
+++ Array.java
@@ -1,8 +1,8 @@
 public class Array {
   public static void main(String args[]){
-    int num[] = {3,5,7,9};
+    int num[] = new int[5];
 
-    num[1] = 6;
+    //num[1] = 6;
 
     System.out.println(num[1]);
   }
