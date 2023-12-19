8c588092a75319d1813f966562adba1745230d13
ajayravriya <ajayravriya14325@gmail.com>
2023-12-19 22:31:13 +0530

    2d array for loop enhancement

--- TwoDArray.java
+++ TwoDArray.java
@@ -5,7 +5,20 @@ public class TwoDArray {
     for(int i=0;i<3;i++){
       for(int j=0;j<4;j++){
         nums[i][j] = (int)(Math.random() * 10);
+      }
+    }
+
+    for(int i=0;i<3;i++){
+      for(int j=0;j<4;j++){
+        nums[i][j] = (int)(Math.random() * 10);
         System.out.print(nums[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    for(int n[] : nums){
+      for(int m : n){
+        System.out.print(m + " ");
       }
       System.out.println();
     }
