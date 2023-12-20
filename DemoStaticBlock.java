48d5bff0caaf0017b15158d035dc3175e708fe77
ajayravriya <ajayravriya14325@gmail.com>
2023-12-20 23:03:42 +0530

    Static block called via Class.forName() load in java

--- DemoStaticBlock.java
+++ DemoStaticBlock.java
@@ -25,19 +25,8 @@ class Mobile{
 
 public class DemoStaticBlock {
   
-  public static void main(String a[]){
-    Mobile m1 = new Mobile();
-    m1.brand = "Apple";
-    m1.price = 80000;
-    // static var should be called via class name not object name
-    Mobile.name = "SmartPhone";
-
-    Mobile m2 = new Mobile();
-
-    Mobile m3 = new Mobile();
-    m3.brand = "One plus";
-    m3.price = 30000;
-    Mobile.name = "SmartPhone";
-
+  public static void main(String a[]) throws ClassNotFoundException{
+    //Now static block is called
+    Class.forName("Mobile");
   }
 }
