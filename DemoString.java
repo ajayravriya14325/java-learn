c033169681594a6ad0d5098a19b79c9e7975b596
ajayravriya <ajayravriya14325@gmail.com>
2023-12-20 18:56:12 +0530

    Strings are immutable

--- DemoString.java
+++ DemoString.java
@@ -1,7 +1,8 @@
 public class DemoString {
+  //Strings are immutable
   public static void main(String a[]){
     String name = "Ajay";
-    name = name + " Ravriya";
+    name = name + " Ravriya"; // you are changing referance address Ajay to Ajay Ravriya
     System.out.println(name);
 
     String s1 = "Ajay"; //line no 7 and 8 both are having same string value means both the value refering to one object not two diffrent object
