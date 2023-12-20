ba8980366c6500e30187ac6e948a8365cad0bb46
ajayravriya <ajayravriya14325@gmail.com>
2023-12-20 18:24:19 +0530

    String use some built in methods

--- DemoString.java
+++ DemoString.java
@@ -1,8 +1,10 @@
 public class DemoString {
   public static void main(String a[]){
-    String name = "Ajay"; // this is not correct becoz String is a class
-    String name1 = new String("Vijay");
+    String name = "Ajay"; // Sortcut most of the this syntax used
+    String name1 = new String("Vijay"); // this is correct becoz String is a class
     System.out.println(name + " " + name1);
     System.out.println(name.hashCode());
+    System.out.println(name.charAt(0));
+    System.out.println(name.concat(" and ") + name1);
   }
 }
