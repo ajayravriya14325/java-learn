202590bfcb332e617503142b1b464a1b5e6cc43d
ajayravriya <ajayravriya14325@gmail.com>
2023-12-21 21:41:45 +0530

    use of this keyword in java.

--- Encapsulation.java
+++ Encapsulation.java
@@ -7,16 +7,18 @@ class Human{
     return age;
   }
 
-  public void setAge(int a){
-    age = a;
+  public void setAge(int age,Human obj){
+    Human obj1 = obj; //this is not correct in java instance of we can use this keyword
+    obj1.age = age;
   }
 
   public String getName(){
     return name;
   }
 
-  public void setName(String n){
-    name = n;
+  public void setName(String name){
+    //this refers current object who is calling the method.
+    this.name = name;
   }
 }
 
@@ -25,7 +27,7 @@ public class Encapsulation {
 
     Human h1 = new Human();
     //Set private var value via method
-    h1.setAge(20);
+    h1.setAge(20,h1);
     h1.setName("Ravriya");
 
     //private var we can access via method call outside the class
