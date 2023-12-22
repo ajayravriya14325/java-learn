32e9e865ade8b339a618d69e76e62d4f4d05847f
ajayravriya <ajayravriya14325@gmail.com>
2023-12-22 21:15:43 +0530

    Anonymous Object in java

---
+++ AnonymousObject.java
@@ -0,0 +1,16 @@
+class A{
+  public A(){
+    System.out.println("Object is created");
+  }
+
+  public void show(){
+    System.out.println("In A show");
+  }
+}
+
+public class AnonymousObject {
+  public static void main(String a[]){
+    new A(); // this is Anonymous Object becoz don't have name
+    new A().show();
+  }
+}
