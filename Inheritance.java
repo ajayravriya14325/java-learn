38e29c0b917d00b5a53693c8ddb736651d9dee46
ajayravriya <ajayravriya14325@gmail.com>
2023-12-22 22:02:49 +0530

    Multi level inheritance in java

--- Inheritance.java
+++ Inheritance.java
@@ -1,11 +1,13 @@
 public class Inheritance {
   public static void main(String[] args) {
-    AdvanceCalc obj1 = new AdvanceCalc();
+    VeryAdvanceCalc obj1 = new VeryAdvanceCalc();
+
     int r1 = obj1.add(5, 9);
     int r2 = obj1.sub(15, 9);
     int r3 = obj1.multi(5, 9);
     int r4 = obj1.div(15, 9);
+    double r5 = obj1.power(4,2);
 
-    System.out.println(r1+" "+r2+" "+r3+" "+r4);
+    System.out.println(r1+" "+r2+" "+r3+" "+r4+" "+r5);
   }
 }
