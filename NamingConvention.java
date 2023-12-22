3c8b1a0d989d1a2408613ca37772ddeafe7f333e
ajayravriya <ajayravriya14325@gmail.com>
2023-12-22 21:03:31 +0530

    Naming Conventions in java

---
+++ NamingConvention.java
@@ -0,0 +1,11 @@
+public class NamingConvention {
+  
+}
+
+//Camel casing
+// class = Human
+//interface = Runable
+// constructor = Human()
+// variables = age, name
+// method = setAge(), age()
+// constants = PIE, BRAND
