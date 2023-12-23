4e132df72c1b66761c45c04a7cacd237392e19cd
ajayravriya <ajayravriya14325@gmail.com>
2023-12-23 21:33:19 +0530

    Final variable in java

---
+++ DemoFinal.java
@@ -0,0 +1,9 @@
+//final variables, method and class final keyword for creating constant we can't change the value
+
+public class DemoFinal {
+  public static void main(String[] args) {
+    final int num = 15;
+    num = 4;
+    System.out.println(num);
+  }
+}
