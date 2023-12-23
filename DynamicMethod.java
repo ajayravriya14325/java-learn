3a6850a01b606db7c4fcea5cf632c4dd55822c39
ajayravriya <ajayravriya14325@gmail.com>
2023-12-23 14:29:16 +0530

    Runtime poly morphism in java

---
+++ DynamicMethod.java
@@ -0,0 +1,32 @@
+class A {
+  public void show(){
+    System.out.println("In A show");
+  }
+}
+
+class B extends A{
+  public void show(){
+    System.out.println("In B show");
+  }
+}
+
+class C extends B{
+  public void show(){
+    System.out.println("In C show");
+  }
+}
+
+
+public class DynamicMethod {
+  public static void main(String[] args) {
+    //Run time ploymorphism
+    A obj = new A();
+    obj.show();
+
+    obj = new B();
+    obj.show();
+
+    obj = new C();
+    obj.show();
+  }
+}
