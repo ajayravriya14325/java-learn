cd450a1cca85a386bcad986fa8bd476363d90ccf
ajayravriya <ajayravriya14325@gmail.com>
2023-12-23 22:09:07 +0530

    Object class hascode in java

---
+++ ObjectClass.java
@@ -0,0 +1,12 @@
+class Laptop{
+  String model;
+  int price;
+}
+
+public class ObjectClass {
+  public static void main(String[] args) {
+    Laptop obj = new Laptop();
+    System.out.println(obj);
+    System.out.println(obj.toString());
+  }
+}
