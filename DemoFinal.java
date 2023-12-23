3ddc1f95bc6d935a709f98fa8fe77329db5bdc85
ajayravriya <ajayravriya14325@gmail.com>
2023-12-23 21:42:28 +0530

    Final class in java

--- DemoFinal.java
+++ DemoFinal.java
@@ -1,9 +1,23 @@
-//final variables, method and class final keyword for creating constant we can't change the value
+//final variables, method and class 
+//final variables for creating constant we can't change the value
+//final class stop class the inheritance
+final class Calc{
+  public void show(){
+    System.out.println("By Ajay");
+  }
+  public int add(int a,int b){
+    System.out.println(a+b);
+  }
+}
+
+class AdvCalc extends Calc{
+
+}
 
 public class DemoFinal {
   public static void main(String[] args) {
-    final int num = 15;
-    num = 4;
-    System.out.println(num);
+    AdvCalc obj = new AdvCalc();
+    obj.show();
+    obj.add(5, 3);
   }
 }
