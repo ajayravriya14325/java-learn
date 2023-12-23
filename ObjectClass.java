513e28fb6e7822d70c97aa49f9e366f34cc14e60
ajayravriya <ajayravriya14325@gmail.com>
2023-12-23 22:27:50 +0530

    Over ride object class equals method in java

--- ObjectClass.java
+++ ObjectClass.java
@@ -5,6 +5,14 @@ class Laptop{
   public String toString(){
     return model + " : " + price;
   }
+
+  public boolean equals(Laptop that){
+    if(this.model.equals(that.model) && this.price == that.price){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
 
 public class ObjectClass {
@@ -13,6 +21,12 @@ public class ObjectClass {
     obj.model = "Lenovo Yoga";
     obj.price = 10000;
 
-    System.out.println(obj.toString());
+    Laptop obj1 = new Laptop();
+    obj1.model = "Lenovo Yoga";
+    obj1.price = 10000;
+
+    boolean result = obj.equals(obj1);
+
+    System.out.println(result);
   }
 }
