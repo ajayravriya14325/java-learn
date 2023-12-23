de66f85fc97a7f9e981b1f3979b63fd30e2796e2
ajayravriya <ajayravriya14325@gmail.com>
2023-12-23 22:18:09 +0530

    Over ride object class toString method in java

--- ObjectClass.java
+++ ObjectClass.java
@@ -1,12 +1,18 @@
 class Laptop{
   String model;
   int price;
+
+  public String toString(){
+    return model + " : " + price;
+  }
 }
 
 public class ObjectClass {
   public static void main(String[] args) {
     Laptop obj = new Laptop();
-    System.out.println(obj);
+    obj.model = "Lenovo Yoga";
+    obj.price = 10000;
+
     System.out.println(obj.toString());
   }
 }
