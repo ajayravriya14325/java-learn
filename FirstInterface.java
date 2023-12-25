5b97b65de7e2b5ad9966ff6fdbc97e8ba2524892
ajayravriya <ajayravriya14325@gmail.com>
2023-12-25 22:03:46 +0530

    Multiple interface in java

--- FirstInterface.java
+++ FirstInterface.java
@@ -7,7 +7,12 @@ interface A {
   void config();
 }
 
-class B implements A{
+interface X{
+
+  void run();
+}
+
+class B implements A,X{
 
   public void show(){
     System.out.println("in show");
@@ -17,16 +22,21 @@ class B implements A{
     System.out.println("in config");
   }
 
+  public void run(){
+    System.out.println("Running");
+  }
+
 }
 
 
 public class FirstInterface {
   public static void main(String[] args) {
     A obj;
-
     obj = new B();
+    X obj1 = new B();
     obj.show();
     obj.config();
+    obj1.run();
 
     System.out.println("Age : "+A.age+ " Area : "+A.area);
   }
