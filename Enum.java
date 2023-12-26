f21fb910d041475d78fa295763f0842eaad67fb6
ajayravriya <ajayravriya14325@gmail.com>
2023-12-26 22:00:21 +0530

    Enum with Switch case in java

--- Enum.java
+++ Enum.java
@@ -10,14 +10,19 @@ public class Enum {
     //accessing enum class object
     Status s = Status.Running;
 
-    if(s == Status.Running){
-      System.out.println("All Good");
-    }else if (s == Status.Failed) {
-      System.out.println("Try again");
-    }else if(s == Status.Pending){
-      System.out.println("Please wait");
-    }else{
-      System.out.println("Done");
+    switch (s) {
+      case Running:
+        System.out.println("All Good");
+        break;
+      case Failed:
+        System.out.println("Try again");
+        break;
+      case Pending:
+        System.out.println("Please wait");
+        break;
+      default:
+        System.out.println("Done");
+        break;
     }
   }
 }
