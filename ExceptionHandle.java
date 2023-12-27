c809221619b062eba5fc6c82b94a850c2c77164f
ajayravriya <ajayravriya14325@gmail.com>
2023-12-27 22:44:02 +0530

    Try with multiple catch in java exception

--- ExceptionHandle.java
+++ ExceptionHandle.java
@@ -4,8 +4,20 @@ public class ExceptionHandle {
     int i =0;
     int j = 0;
 
+    int nums[] = new int[5];
+    String str = null;
+
     try{
       j = 18/i;
+      System.out.println(str.length());
+      System.out.println(nums[1]);
+      System.out.println(nums[5]);
+    }catch(ArithmeticException e){
+      System.out.println("Can not divide by zero.");
+    }catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("Stay in your limit.");
+    }catch(NullPointerException e){
+      System.out.println("str is null.");
     }catch(Exception e){
       System.out.println("Something went wrong...");
     }
