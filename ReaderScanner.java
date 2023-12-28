22e151271df7c2e19546b4908c831a56fa14e6a6
ajayravriya <ajayravriya14325@gmail.com>
2023-12-28 22:46:48 +0530

    Scanner Input in java

--- ReaderScanner.java
+++ ReaderScanner.java
@@ -1,18 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ReaderScanner {
   public static void main(String[] args) throws IOException{
     
     System.out.println("Enter a number");
 
-    InputStreamReader in = new InputStreamReader(System.in);
-    BufferedReader bf = new BufferedReader(in);
+    //InputStreamReader in = new InputStreamReader(System.in);
+    //BufferedReader bf = new BufferedReader(in);
 
-    int num = Integer.parseInt(bf.readLine());
+    //int num = Integer.parseInt(bf.readLine());
+
+    Scanner sc = new Scanner(System.in);
+
+    int num = sc.nextInt();
+
     System.out.println(num);
 
-    bf.close();
+    //bf.close();
   }
 }
