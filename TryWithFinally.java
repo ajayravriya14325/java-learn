894f57d7b5764f1a40102ad201328f7d6a393330
ajayravriya <ajayravriya14325@gmail.com>
2023-12-29 22:26:41 +0530

    try with resources in java

--- TryWithFinally.java
+++ TryWithFinally.java
@@ -7,17 +7,13 @@ public class TryWithFinally {
   public static void main(String[] args) throws NumberFormatException, IOException {
     
     int nums = 0;
-    BufferedReader br = null;
-    try{
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in)))
+    {
       System.out.println("Enter a number");
-      br = new BufferedReader(new InputStreamReader(System.in));
       nums = Integer.parseInt(br.readLine());
 
       System.out.println(nums);
 
-    }
-    finally{ //finally block excute in both case try as well as catch
-      br.close(); //finally is used to close the resource file,network & database connection, input
     }
   }
   
