f74137c7a066c849d8be075fceada900828e15a9
ajayravriya <ajayravriya14325@gmail.com>
2023-12-29 23:18:49 +0530

    Thread with Runnable interface in java

---
+++ RunnableWithThread.java
@@ -0,0 +1,45 @@
+
+class A implements Runnable {
+  public void run(){
+    for(int i = 0; i < 5; i++){
+      System.out.println("Hi");
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+//in thread method name is run always 
+class B implements Runnable {
+  public void run(){
+    for(int i = 0; i < 5; i++){
+      System.out.println("Hello");
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+public class RunnableWithThread {
+  public static void main(String[] args) {
+    
+    Runnable obj1 = new A();
+    Runnable obj2 = new B();
+
+    Thread t1 = new Thread(obj1);
+    Thread t2 = new Thread(obj2);
+
+    t1.start(); //thread method called via start()
+    t2.start();
+  }
+}
+
+
