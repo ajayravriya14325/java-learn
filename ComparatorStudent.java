ea1f9c516641ed322cd466a561abbf08d5fcb60c
ajayravriya <ajayravriya14325@gmail.com>
2023-12-30 16:49:03 +0530

    Comparator with Object data in java

---
+++ ComparatorStudent.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Student{
+  int age;
+  String name;
+  
+  public Student(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Student [age=" + age + ", name=" + name + "]";
+  }
+  
+}
+
+public class ComparatorStudent {
+  public static void main(String[] args) {
+
+    Comparator<Student> com = new Comparator<Student>() {
+      public int compare(Student i, Student j) {
+        if(i.age > j.age){
+          return 1;
+        }else{
+          return -1;
+        }
+      }
+    };
+    
+    List<Student> studs = new ArrayList<>();
+    studs.add(new Student(21,"Navin"));
+    studs.add(new Student(18,"John"));
+    studs.add(new Student(12,"Kiran"));
+    studs.add(new Student(25,"Ajay"));
+
+    Collections.sort(studs, com);
+    for(Student s : studs){
+      System.out.println(s);
+    }
+  }
+}
