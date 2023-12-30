1371c7fd3c739c8f2ff5d82500dd7f66119ca5a9
ajayravriya <ajayravriya14325@gmail.com>
2023-12-30 22:14:18 +0530

    Sream api in java

---
+++ SreamApi.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class SreamApi {
+  public static void main(String[] args) {
+    
+    List<Integer> nums = Arrays.asList(4,5,7,3,2,6);
+
+    //Stream can't change the nums value and can't reuse it.
+    Stream<Integer> s1 = nums.stream();
+    Stream<Integer> s2 = s1.filter(n -> n%2 == 0);
+    Stream<Integer> s3 = s2.map(n -> n*n);
+    int result = s3.reduce(0, (c,e) -> c+e);
+
+    System.out.println(result);
+
+    //s3.forEach(t -> System.out.println(t)); 
+    //s2.forEach(t -> System.out.println(t));
+    //s1.forEach(t -> System.out.println(t));
+  }
+}
